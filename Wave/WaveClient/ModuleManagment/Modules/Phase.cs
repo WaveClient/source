45fd4e09aae1b4d1c2c61a1cd215fcbd3871dbf3
A9039 <66700605+A9039@users.noreply.github.com>
2020-11-09 13:51:06 -0500

    Update Phase.cs

--- Wave/WaveClient/ModuleManagment/Modules/Phase.cs
+++ Wave/WaveClient/ModuleManagment/Modules/Phase.cs
@@ -15,20 +15,20 @@ namespace WaveClient.Module
     {
         public static bool ToggleState;
 
-        static Pointer yPos = new Pointer("Minecraft.Windows.exe", 0x036A0238, new int[] { 0x10, 0x18, 0x80, 0x9F8, 0x18, 0x45C});
+        static Pointer ypos = new Pointer("Minecraft.Windows.exe", 0x036A0238, new int[] { 0x10, 0x18, 0x80, 0x9F8, 0x18, 0x45C});
+        static float ha = Memory0.mem.ReadFloat(ypos);
+        static float Y2 = ha + 1.8f;
 
        // static float currentYPos = Memory0.mem.ReadFloat(yPos);
 
-        public static void Tick10()
-        {
-
-        }
-
         public static void Enable()
         {
-            //Console.WriteLine(currentYPos);
-            Console.WriteLine("Sadly this is broke at the moment, we are working to fix this, -The Wave Development Team");
-            //Memory0.mem.WriteMemory(yPos, currentYPos + 2);
+            Y2 
+        }
+
+        public static void Disable()
+        {
+            Y2 = ha + 1.8f;
         }
 
     }
