61a2c08b17a1e5572cf8456ab07477152796bb53
A9039 <66700605+A9039@users.noreply.github.com>
2020-11-09 14:08:13 -0500

    I am getting better making these modules better :D
    Slfnjv fkj brkjbgr kb n brjgbj

--- Wave/WaveClient/ModuleManagment/Modules/KillYourSelf.cs
+++ Wave/WaveClient/ModuleManagment/Modules/KillYourSelf.cs
@@ -10,13 +10,12 @@ namespace WaveClient.Module
     {
         public static bool ToggleState;
 
-        static Pointer kys = new Pointer("Minecraft.Windows.exe", 0x036A0278, new int[] { 0x8, 0x18, 0x80, 0x5B0, 0xB0, 0xF8, 0x19C });
         static Pointer ypos = new Pointer("Minecraft.Windows.exe", 0x036A0238, new int[] { 0x10, 0x18, 0x80, 0x9F8, 0x18, 0x45C});
         static float cypos = Memory0.mem.ReadFloat(ypos);
         static float dropvalue = cypos + 30;
         public static void Enable()
         {
-          Memory0.mem.WriteMemory(kys, dropvalue);
+          Memory0.mem.WriteMemory(ypos, dropvalue);
         }
     }
     
