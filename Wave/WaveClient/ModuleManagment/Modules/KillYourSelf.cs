ac32dc80b218a5c0f10af37842c25718b155513b
A9039 <66700605+A9039@users.noreply.github.com>
2020-11-10 09:35:01 -0500

    i wanna make a client in js ngl

--- Wave/WaveClient/ModuleManagment/Modules/KillYourSelf.cs
+++ Wave/WaveClient/ModuleManagment/Modules/KillYourSelf.cs
@@ -12,12 +12,14 @@ namespace WaveClient.Module
 
         static Pointer ypos = new Pointer("Minecraft.Windows.exe", 0x036A0278, new int[] { 0x8, 0x18, 0x80, 0x5B0, 0xB0, 0xF8, 0X45C });
         static float cypos;
+        
         public static void Enable()
         {
-            cypos = Memory0.mem.ReadFloat(ypos);
+            float cypos = Memory0.mem.ReadFloat(ypos);
             float dropvalue = cypos + 30f;
-            Console.WriteLine("The ypos is " + cypos + "and the drop value is " + dropvalue);
+            Console.WriteLine("The ypos is " + cypos;
             Memory0.mem.WriteMemory(ypos, dropvalue);
+            
         }
     }
     
