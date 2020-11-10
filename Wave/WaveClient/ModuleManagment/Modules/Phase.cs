0b0a51491c401f762e435ab86afc130a699de1a3
A9039 <66700605+A9039@users.noreply.github.com>
2020-11-10 09:53:12 -0500

    Last change, (hopefully)

--- Wave/WaveClient/ModuleManagment/Modules/Phase.cs
+++ Wave/WaveClient/ModuleManagment/Modules/Phase.cs
@@ -16,7 +16,7 @@ namespace WaveClient.Module
         public static bool ToggleState;
 
         static Pointer ypos = new Pointer("Minecraft.Windows.exe", 0x036A0278, new int[] { 0x8, 0x18, 0x80, 0x5B0, 0xB0, 0xF8, 0X45C });
-        //static Pointer ypos2 = new Pointer("Minecraft.Windows.exe", 0x036A0278, new int[] { 0x8, 0x18, 0x80, 0x5B0, 0xB0, 0xF8, 0X45C + 12 });
+        static Pointer ypos2 = new Pointer("Minecraft.Windows.exe", 0x036A0278, new int[] { 0x8, 0x18, 0x80, 0x5B0, 0xB0, 0xF8, 0X45C + 12 });
         //static Pointer X1 = new Pointer("Minecraft.Windows.exe", 0x30366F8, new int[] { 0x0 - 4 });
         //static Pointer Z1 = new Pointer("Minecraft.Windows.exe", 0x30366F8, new int[] { 0x0 + 4 });
         //static Pointer X2 = new Pointer("Minecraft.Windows.exe", 0x30366F8, new int[] { 0x0 + 8 });
@@ -24,20 +24,26 @@ namespace WaveClient.Module
         static float Y1 = Memory0.mem.ReadFloat(ypos);
         static float Y2 = Memory0.mem.ReadFloat(ypos2);
 
-       // static float currentYPos = Memory0.mem.ReadFloat(yPos);
-
+        public static void findypos()
+        {
+            float Y1;
+            Y1 = Memory0.mem.ReadFloat(ypos);
+            return Y1;
+        }
+        //
         public static void Tick10()
         {
+            findypos();
             float Y2 = Y1;
             Memory0.mem.WriteMemory(ypos, Y2);
         }
-
+        //
         public static void Disable()
         {
             float Y2 = Y1 + 1.8f;
             Memory0.mem.WriteMemory(ypos, Y2);
         }
-
+        //
     }
 
 }
