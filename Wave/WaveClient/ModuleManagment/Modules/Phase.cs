d7117741c8a1af793c19f5ec046d1f1c907f886f
A9039 <66700605+A9039@users.noreply.github.com>
2020-11-10 09:54:39 -0500

    Correct the pointer SMH

--- Wave/WaveClient/ModuleManagment/Modules/Phase.cs
+++ Wave/WaveClient/ModuleManagment/Modules/Phase.cs
@@ -17,10 +17,10 @@ namespace WaveClient.Module
 
         static Pointer ypos = new Pointer("Minecraft.Windows.exe", 0x036A0278, new int[] { 0x8, 0x18, 0x80, 0x5B0, 0xB0, 0xF8, 0X45C });
         static Pointer ypos2 = new Pointer("Minecraft.Windows.exe", 0x036A0278, new int[] { 0x8, 0x18, 0x80, 0x5B0, 0xB0, 0xF8, 0X45C + 12 });
-        //static Pointer X1 = new Pointer("Minecraft.Windows.exe", 0x30366F8, new int[] { 0x0 - 4 });
-        //static Pointer Z1 = new Pointer("Minecraft.Windows.exe", 0x30366F8, new int[] { 0x0 + 4 });
-        //static Pointer X2 = new Pointer("Minecraft.Windows.exe", 0x30366F8, new int[] { 0x0 + 8 });
-        //static Pointer Z2 = new Pointer("Minecraft.Windows.exe", 0x30366F8, new int[] { 0x0 + 13 });
+        //static Pointer X1 = new Pointer("Minecraft.Windows.exe", 0x30366F8, new int[] { 0x8, 0x18, 0x80, 0x5B0, 0xB0, 0xF8, 0X45C - 4 });
+        //static Pointer Z1 = new Pointer("Minecraft.Windows.exe", 0x30366F8, new int[] { 0x8, 0x18, 0x80, 0x5B0, 0xB0, 0xF8, 0X45C + 4 });
+        //static Pointer X2 = new Pointer("Minecraft.Windows.exe", 0x30366F8, new int[] { 0x8, 0x18, 0x80, 0x5B0, 0xB0, 0xF8, 0X45C + 8 });
+        //static Pointer Z2 = new Pointer("Minecraft.Windows.exe", 0x30366F8, new int[] { 0x8, 0x18, 0x80, 0x5B0, 0xB0, 0xF8, 0X45C + 13 });
         static float Y1 = Memory0.mem.ReadFloat(ypos);
         static float Y2 = Memory0.mem.ReadFloat(ypos2);
 
