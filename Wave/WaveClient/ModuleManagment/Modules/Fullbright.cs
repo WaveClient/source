9e3e8d2d623cbc872ac6eb077be0e6b393ae31cc
A9039 <66700605+A9039@users.noreply.github.com>
2020-11-10 10:41:33 -0500

    Update Fullbright.cs

--- Wave/WaveClient/ModuleManagment/Modules/Fullbright.cs
+++ Wave/WaveClient/ModuleManagment/Modules/Fullbright.cs
@@ -14,7 +14,7 @@ namespace WaveClient.Module
         static Pointer fullbrightptr = new Pointer("Minecraft.Windows", 0x, new int[] { 0x0 });
 
 
-        public static void Tick100()
+        public static void Tick1000()
         {
             Console.WriteLine("Coming soon!");
             //Memory0.mem.WriteMemory(fullbrightptr, 100f);
