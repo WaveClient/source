790aadb966f60d54d46ee887a56f3079ebe8fa62
A9039 <66700605+A9039@users.noreply.github.com>
2020-11-10 10:38:16 -0500

    ez fullbright xd

--- Wave/WaveClient/ModuleManagment/Modules/Fullbright.cs
+++ Wave/WaveClient/ModuleManagment/Modules/Fullbright.cs
@@ -1,1 +1,25 @@
+using System;
+using WaveClient.ModuleManagment;
+using Wave.Cmr.MemoryManagement;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using System.Windows.Forms;
 
+namespace WaveClient.Module
+{
+
+    public static class Fullbright
+    {
+        public static bool ToggleState;
+        static Pointer fullbrightptr = new Pointer("Minecraft.Windows", 0x, new int[] { 0x0 });
+
+
+        public static void Tick100()
+        {
+            Console.WriteLine("Coming soon!");
+            //Memory0.mem.WriteMemory(fullbrightptr, 100f);
+        }
+
+    }
+
+}
