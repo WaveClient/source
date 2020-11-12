1dd27a36a00155da456a72af110c7b852a4cc746
A9039 <66700605+A9039@users.noreply.github.com>
2020-11-12 13:24:44 -0500

    Update AutoSneak.cs

--- Wave/WaveClient/ModuleManagment/Modules/AutoSneak.cs
+++ Wave/WaveClient/ModuleManagment/Modules/AutoSneak.cs
@@ -11,16 +11,13 @@ namespace WaveClient.Module
 
         static Pointer autosneakptr = new Pointer("Minecraft.Windows.exe", 0x10513A0, new int[] { 0xEB });
         static Pointer autosneakptrdisabled = new Pointer("Minecraft.Windows.exe", 0x10513A0, new int[] { 0x75 });
+        static float sneakvalue;
 
         public static void Enable()
         {
 
-            Memory0.mem.WriteMemory(autosneakptr);
+            Memory0.mem.WriteMemory(autosneakptr, sneakvalue);
         }
         
-        public static void Disable()
-        {
-        Memory0.mem.WriteMemory(autosneakptrdisabled);
-        }
     }
     
