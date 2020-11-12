f32641aa962c77f5a0de0fd769cb2fe2223ba122
A9039 <66700605+A9039@users.noreply.github.com>
2020-11-12 09:36:47 -0500

    Update AutoWalk.cs

--- Wave/WaveClient/ModuleManagment/Modules/AutoWalk.cs
+++ Wave/WaveClient/ModuleManagment/Modules/AutoWalk.cs
@@ -11,15 +11,21 @@ namespace WaveClient.Module
     {
         public static bool ToggleState;
 
-        //static Pointer yaw = new Pointer("Minecraft.Windows.exe", 0x0, new int[] { 0x0 });
-        public static void Enable()
+        static Pointer yawptr = new Pointer("Minecraft.Windows.exe", 0x0, new int[] { 0x0 });
+        static Pointer yvelo = new Pointer("Minecraft.Windows.exe", 0x036A0288, new int[] { 0x68, 0x8, 0x18, 0x88, 0x80, 0xBB8, 0x498 });
+        static Pointer velx = new Pointer("Minecraft.Windows.exe", 0x036A0288, new int[] { 0x68, 0x8, 0x18, 0x88, 0x80, 0xBB8, 0x498 - 4 });
+        static Pointer velz = new Pointer("Minecraft.Windows.exe", 0x036A0288, new int[] { 0x68, 0x8, 0x18, 0x88, 0x80, 0xBB8, 0x498 + 4 });
+        
+        public static void Tick10()
         {
+            float yaw = Memory0.mem.ReadFloat(yawptr);
             float cYaw = (yaw + 90f) * ((float)Math.PI / 180f);
             
             float walkval = 1f;
-            float velX = (float)Math.Cos(cYaw) * walkval;
-            float velZ = (float)Math.Sin(cYaw) * walkval;
-            Memory0.mem.WriteMemory(yaw, velX);
+            float velXval = (float)Math.Cos(cYaw) * walkval;
+            float velZval = (float)Math.Sin(cYaw) * walkval;
+            Memory0.mem.WriteMemory(velx, velXval);
+            Memory0.mem.WriteMemoy(velz, velZval);
         }
     }
     
