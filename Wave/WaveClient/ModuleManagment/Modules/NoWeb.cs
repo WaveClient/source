dedd8e6f85814e79535371e7ac9c9030e494feee
A9039 <66700605+A9039@users.noreply.github.com>
2020-11-12 09:51:47 -0500

    Update NoWeb.cs

--- Wave/WaveClient/ModuleManagment/Modules/NoWeb.cs
+++ Wave/WaveClient/ModuleManagment/Modules/NoWeb.cs
@@ -9,7 +9,7 @@ namespace WaveClient.Module
     {
         public static bool ToggleState;
 
-        static Pointer nowebptr = new Pointer("Minecraft.Windows.exe", 0x036A0278, new int[] { 0x8, 0x18, 0x80, 0x5B0, 0xB0, 0xF8, 0X45C - 4 });
+        static Pointer nowebptr = new Pointer("Minecraft.Windows.exe", 0x12FE2A5, new int[] { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 });
 
         public static void Enable()
         {
